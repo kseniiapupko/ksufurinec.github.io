dd1ca0c3ed57046e313ef4c3f907ee1a863a0caf
ksufurinec <ksuwa_2008@ukr.net>
2015-11-30 17:48:32 +0200

    Create pow.js

---
+++ GoIT/JavaScript/Home/Lesson1-2/JavaScript/pow.js
@@ -0,0 +1,7 @@
+var a = prompt ('Укажите число');
+console.log ("a=", a)
+var b = prompt ('Укажите степень');
+console.log ("b=", b)
+
+Math.pow(a,b);
+console.log ('Результат возведения в степень =', Math.pow(a,b) )
