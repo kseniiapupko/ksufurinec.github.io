5b6ac5a9f2731c2ef13c81029aef0f920458b17a
ksufurinec <ksuwa_2008@ukr.net>
2015-12-03 12:18:52 +0200

    Update pow.js

--- GoIT/JavaScript/Home/Lesson1-2/JavaScript/pow.js
+++ GoIT/JavaScript/Home/Lesson1-2/JavaScript/pow.js
@@ -1,7 +1,15 @@
-var a = prompt ('Укажите число');
-console.log ("a=", a)
-var b = prompt ('Укажите степень');
-console.log ("b=", b)
+var x = prompt ('Укажите число');
+console.log ("x=", x)
+var n = prompt ('Укажите степень');
+console.log ("n=", n)
 
-Math.pow(a,b);
-console.log ('Результат возведения в степень =', Math.pow(a,b) )
+// Math.pow(x,n);
+// console.log ('Результат возведения в степень =', Math.pow(x,n) )
+
+var result = 1;
+
+for (var i=0; i<n; i++){
+	result=result * x;
+}
+
+console.log(result);
